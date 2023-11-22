61f5622eaf32c0c7c5e8dca6212d54a61d33bebc
javitorcon <javiertc7300@gmail.com>
2023-11-22 13:28:53 +0100

    creado el esquema global para traducir a bd global

--- Hard Skool/GlobalModel.cs
+++ Hard Skool/GlobalModel.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Hard_Skool
 {
-    internal class GlobalModel
+    public class GlobalModel
     {
+        public enum TipoCentro
+        {
+            Público,
+            Concertado,
+            Privado,
+            Otros
+        }
+
+        // Modelo para Provincia
+        public class Provincia
+        {
+            public int Codigo { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        // Modelo para Localidad
+        public class Localidad
+        {
+            public int Codigo { get; set; }
+            public string Nombre { get; set; }
+            public int EnProvincia { get; set; }
+        }
+
+        // Modelo para Centro_Educativo
+        public class CentroEducativo
+        {
+            public int IdLocal { get; set; }
+            public string Origen { get; set; }
+            public string Nombre { get; set; }
+            public TipoCentro Tipo { get; set; }
+            public string Direccion { get; set; }
+            public int CodigoPostal { get; set; }
+            public float Longitud { get; set; }
+            public float Latitud { get; set; }
+            public int Telefono { get; set; }
+            public string Descripcion { get; set; }
+            public int EnLocalidad { get; set; }
+        }
     }
 }
